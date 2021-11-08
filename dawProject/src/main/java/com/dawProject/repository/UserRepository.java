ff8f3dac7b8af9bb541aba72a5833942698fc975
David <alum.dgonzalezc@iesalixar.org>
2021-11-08 18:50:29 +0100

    08nov

--- dawProject/src/main/java/com/dawProject/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dawProject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dawProject.model.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-	
-}
